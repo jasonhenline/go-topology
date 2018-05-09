969ad700c9dd4892c937f5bd4f8229cdbd2ea76a
Fred Allen <fred.p.allen@gmail.com>
2018-05-09 13:39:23 -0700

    Lint cleanup and improve score display.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+/*jshint esversion: 6 */
+
 class Board {
   // Makes an empty board.
   constructor(width, height) {
     this.turn = "black";
     this.width = width;
     this.height = height;
-    this.prisonersTakenBy = {"black": 0, "white": 0};
+    this.prisonersTakenBy = {black: 0, white: 0};
     this.grid = [];
     for (let row = 0; row < height; row++) {
       this.grid.push(Array(width).fill(null));
@@ -15,37 +17,33 @@ class Board {
   //
   // Returns an empty array if no stones are dead.
   getDeadGroup(x, y) {
-    console.log("called getDeadGroup");
     const allDirections = [
       {x: -1, y: 0}, {x: 1, y: 0}, {x: 0, y: -1}, {x: 0, y: 1}];
 
-    let color = this.grid[x][y];
+    const color = this.grid[x][y];
     if (!color) {
       return [];
     }
 
-    let seen = [{x, y}];
-    let toCheck = [{position: {x, y}, directions: allDirections.slice()}];
+    const seen = [{x, y}];
+    const toCheck = [{position: {x, y}, directions: allDirections.slice()}];
 
     while (toCheck.length > 0) {
-      console.log("seen =", seen);
-      console.log("toCheck =", toCheck);
-      let current = toCheck.pop();
-      console.log("current =", current);
+      const current = toCheck.pop();
       x = current.position.x;
       y = current.position.y;
-      let direction = current.directions.pop();
-      let next = {
+      const direction = current.directions.pop();
+      const next = {
         x: (x + direction.x + this.width)%this.width,
         y: (y + direction.y + this.height)%this.height
       };
-      console.log("next =", next);
-      let nextColor = this.grid[next.x][next.y];
+      const nextColor = this.grid[next.x][next.y];
       if (!nextColor) {
         // Found a liberty, no stones have to die.
         return [];
       } else if (nextColor === color) {
-        if (seen.findIndex(({x, y}) => x === next.x && y === next.y) === -1) {
+        const isNext = ({x, y}) => x === next.x && y === next.y;
+        if (seen.findIndex(isNext) === -1) {
           seen.push({x: next.x, y: next.y});
           toCheck.push({
             position: {x: next.x, y: next.y},
@@ -65,17 +63,16 @@ class Board {
   // If the move is illegal, doesn't update this.turn.
   play(x, y) {
     if (!this.grid[x][y]) {
-      this.grid[x][y] = this.turn
+      this.grid[x][y] = this.turn;
       for (let direction of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
-        let neighborX = (x + direction[0] + this.width)%this.width;
-        let neighborY = (y + direction[1] + this.height)%this.height;
-        console.log("neighbor =", neighborX, neighborY);
+        const neighborX = (x + direction[0] + this.width)%this.width;
+        const neighborY = (y + direction[1] + this.height)%this.height;
         for (let dead of this.getDeadGroup(neighborX, neighborY)) {
           this.grid[dead.x][dead.y] = null;
           this.prisonersTakenBy[this.turn]++;
         }
       }
-      let nextTurn = (this.turn == "black") ? "white" : "black";
+      const nextTurn = (this.turn == "black") ? "white" : "black";
       for (let dead of this.getDeadGroup(x, y)) {
         this.grid[dead.x][dead.y] = null;
         this.prisonersTakenBy[nextTurn]++;
@@ -93,16 +90,16 @@ class View {
   }
 
   drawStone(x, y, color) {
-    let stoneRadius = 0.5*this.sideLength;
-    let boardPixelWidth = this.sideLength*this.board.width;
-    let boardPixelHeight = this.sideLength*this.board.height;
-    let grayStroke = "#666666";
-    let grayColor = color === "black" ? grayStroke : "#f9f9f9";
+    const stoneRadius = 0.5*this.sideLength;
+    const boardPixelWidth = this.sideLength*this.board.width;
+    const boardPixelHeight = this.sideLength*this.board.height;
+    const grayStroke = "#666666";
+    const grayColor = color === "black" ? grayStroke : "#f9f9f9";
 
     for (let row = 0; row < 3; ++row) {
       for (let col = 0; col < 3; ++col) {
-        let centerX = (x + 0.5)*this.sideLength + row*boardPixelHeight;
-        let centerY = (y + 0.5)*this.sideLength + col*boardPixelWidth;
+        const centerX = (x + 0.5)*this.sideLength + row*boardPixelHeight;
+        const centerY = (y + 0.5)*this.sideLength + col*boardPixelWidth;
         this.context.beginPath();
         if (row === 1 && col === 1) {
           this.context.fillStyle = color;
@@ -119,8 +116,8 @@ class View {
   }
 
   drawBoard() {
-    let boardPixelWidth = this.sideLength*this.board.width;
-    let boardPixelHeight = this.sideLength*this.board.height;
+    const boardPixelWidth = this.sideLength*this.board.width;
+    const boardPixelHeight = this.sideLength*this.board.height;
 
     // Blank out the whole canvas.
     this.context.fillStyle = "white";
@@ -144,13 +141,13 @@ class View {
     this.context.strokeStyle = "gray";
 
     for (let row = 0; row < 3*this.board.height; row++) {
-      let y = this.sideLength/2 + row*this.sideLength;
+      const y = this.sideLength/2 + row*this.sideLength;
       this.context.moveTo(0, y);
       this.context.lineTo(3*boardPixelWidth, y);
     }
 
     for (let col = 0; col < 3*this.board.width; col++) {
-      let x = this.sideLength/2 + col*this.sideLength;
+      const x = this.sideLength/2 + col*this.sideLength;
       this.context.moveTo(x, 0);
       this.context.lineTo(x, 3*boardPixelHeight);
     }
@@ -163,13 +160,13 @@ class View {
     this.context.strokeStyle = "black";
 
     for (let row = 0; row < this.board.height; row++) {
-      let y = boardPixelHeight + this.sideLength/2 + row*this.sideLength;
+      const y = boardPixelHeight + this.sideLength/2 + row*this.sideLength;
       this.context.moveTo(boardPixelWidth + 1, y);
       this.context.lineTo(2*boardPixelWidth, y);
     }
 
     for (let col = 0; col < this.board.width; col++) {
-      let x = boardPixelWidth + this.sideLength/2 + col*this.sideLength;
+      const x = boardPixelWidth + this.sideLength/2 + col*this.sideLength;
       this.context.moveTo(x, boardPixelHeight + 1);
       this.context.lineTo(x, 2*boardPixelHeight);
     }
@@ -179,7 +176,7 @@ class View {
     // Stones.
     for (let y = 0; y < this.board.height; y++) {
       for (let x = 0; x < this.board.width; x++) {
-        let color = this.board.grid[x][y];
+        const color = this.board.grid[x][y];
         if (color) {
           this.drawStone(x, y, color);
         }
@@ -187,19 +184,19 @@ class View {
     }
 
     // Display prisoner count.
-    let blackScoreSpan = document.getElementById("prisoners_taken_by_black");
-    blackScoreSpan.innerHTML = this.board.prisonersTakenBy["black"];
-    let whiteScoreSpan = document.getElementById("prisoners_taken_by_white");
-    whiteScoreSpan.innerHTML = this.board.prisonersTakenBy["white"];
+    const blackScoreSpan = document.getElementById("prisoners_taken_by_black");
+    blackScoreSpan.innerHTML = this.board.prisonersTakenBy.black;
+    const whiteScoreSpan = document.getElementById("prisoners_taken_by_white");
+    whiteScoreSpan.innerHTML = this.board.prisonersTakenBy.white;
   }
 
   onMouseClickCallback(mouseEvent) {
-    let boardPixelWidth = this.sideLength*this.board.width;
-    let boardPixelHeight = this.sideLength*this.board.height;
+    const boardPixelWidth = this.sideLength*this.board.width;
+    const boardPixelHeight = this.sideLength*this.board.height;
 
-    let gridOffsetX = (
+    const gridOffsetX = (
       Math.floor((mouseEvent.offsetX - boardPixelWidth)/this.sideLength));
-    let gridOffsetY = (
+    const gridOffsetY = (
       Math.floor((mouseEvent.offsetY - boardPixelHeight)/this.sideLength));
 
     if (
@@ -215,12 +212,12 @@ class View {
   onMouseMoveCallback(mouseEvent) {
     this.drawBoard();
 
-    let boardPixelWidth = this.sideLength*this.board.width;
-    let boardPixelHeight = this.sideLength*this.board.height;
+    const boardPixelWidth = this.sideLength*this.board.width;
+    const boardPixelHeight = this.sideLength*this.board.height;
 
-    let gridOffsetX = (
+    const gridOffsetX = (
       Math.floor((mouseEvent.offsetX - boardPixelWidth)/this.sideLength));
-    let gridOffsetY = (
+    const gridOffsetY = (
       Math.floor((mouseEvent.offsetY - boardPixelHeight)/this.sideLength));
 
     if (
@@ -234,16 +231,16 @@ class View {
 }
 
 window.onload = function() {
-  let canvas = document.getElementById("play_area");
-  let context = canvas.getContext("2d");
+  const canvas = document.getElementById("play_area");
+  const context = canvas.getContext("2d");
 
-  let board = new Board(13, 13);
-  let view = new View(context, board, 20);
+  const board = new Board(13, 13);
+  const view = new View(context, board, 20);
 
   view.drawBoard();
   canvas.addEventListener(
     "mousemove", (mouseEvent) => view.onMouseMoveCallback(mouseEvent));
   canvas.addEventListener(
     "click", (mouseEvent) => view.onMouseClickCallback(mouseEvent));
-}
+};
 
